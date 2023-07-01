00ba7f79920542e2e91e268fdf181a6a64a6d291
gly <993214226@qq.com>
2023-07-01 11:57:58 +0800

    修改训练数据集

--- lib/train/admin/local.py
+++ lib/train/admin/local.py
@@ -1,9 +1,9 @@
 class EnvironmentSettings:
     def __init__(self):
-        self.workspace_dir = 'PATH/AiATrack'  # Base directory for saving network checkpoints
+        self.workspace_dir = '/kaggle/working/AiATrack'  # Base directory for saving network checkpoints
         self.tensorboard_dir = self.workspace_dir  # Directory for tensorboard files
         self.pretrained_networks = self.workspace_dir + '/pretrained_networks'
-        self.lasot_dir = 'PATH/LaSOT'
-        self.got10k_dir = 'PATH/GOT10k/train'
-        self.trackingnet_dir = 'PATH/TrackingNet'
-        self.coco_dir = 'PATH/COCO'
+        self.lasot_dir = '/kaggle/input/LaSOT'
+        self.got10k_dir = '/kaggle/input/GOT10k/train'
+        self.trackingnet_dir = '/kaggle/input/TrackingNet'
+        self.coco_dir = '/kaggle/input//COCO'
